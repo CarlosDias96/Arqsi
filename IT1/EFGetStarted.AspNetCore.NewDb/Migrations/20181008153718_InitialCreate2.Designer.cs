7a4ea3e8656d6899af32bd1f2e42c4d74e27384d
Carlos_Dias <1151124@isep.ipp.pt>
2018-10-08 16:38:23 +0100

    migrations update

--- IT1/EFGetStarted.AspNetCore.NewDb/Migrations/20181008153718_InitialCreate2.Designer.cs
+++ IT1/EFGetStarted.AspNetCore.NewDb/Migrations/20181008153718_InitialCreate2.Designer.cs
@@ -2,14 +2,16 @@
 using EFGetStarted.AspNetCore.NewDb.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFGetStarted.AspNetCore.NewDb.Migrations
 {
     [DbContext(typeof(ArqsiContext))]
-    partial class ArqsiContextModelSnapshot : ModelSnapshot
+    [Migration("20181008153718_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IT1/EFGetStarted.AspNetCore.NewDb/Migrations/20181008153718_InitialCreate2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFGetStarted.AspNetCore.NewDb.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Acabamento",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Acabamento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Material",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    AcabamentoId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Material", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Material_Acabamento_AcabamentoId",
+                        column: x => x.AcabamentoId,
+                        principalTable: "Acabamento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Material_AcabamentoId",
+                table: "Material",
+                column: "AcabamentoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Material");
+
+            migrationBuilder.DropTable(
+                name: "Acabamento");
+        }
+    }
+}
