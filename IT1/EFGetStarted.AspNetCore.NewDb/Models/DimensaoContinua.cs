42739747ef4ca319c44be22c587604200f7807c2
DIOGO\diogo <1150802@isep.ipp.pt>
2018-10-09 12:10:26 +0100

    aperfeiçoamento do context, com a inclusão das dimensões contínuas e discretas

---
+++ IT1/EFGetStarted.AspNetCore.NewDb/Models/DimensaoContinua.cs
@@ -0,0 +1,11 @@
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class DimensaoContinua : Dimensao
+    {
+        System.Collections.Generic.List<float> margensAlturas;
+        System.Collections.Generic.List<float> margensProfundidades;
+        System.Collections.Generic.List<float> margensLarguras;
+    }
+
+
+}
--- IT1/EFGetStarted.AspNetCore.NewDb/Models/Model.cs
+++ IT1/EFGetStarted.AspNetCore.NewDb/Models/Model.cs
@@ -9,10 +9,17 @@ namespace  EFGetStarted.AspNetCore.NewDb.Models
         public ArqsiContext(DbContextOptions<ArqsiContext> options)
             : base(options)
         { }
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<DimensaoContinua>();
+            builder.Entity<DimensaoDiscreta>();
+
+            base.OnModelCreating(builder);
+        }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Acabamento> Acabamento { get; set; }
         public DbSet<Material> Material { get; set; }
+        public DbSet<Dimensao> Dimensoes { get; set; }
     }
 
    
