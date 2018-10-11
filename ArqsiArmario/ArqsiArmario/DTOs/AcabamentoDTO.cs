5b5576e3e5dec6facfea0747e7bb7ebf65340dbb
Diogo Soares <Diogomoraisoares@gmail.com>
2018-10-11 19:09:49 +0100

    DTOs adicionados. Faltam as dimensões, que ainda não estão concluidas no Models

---
+++ ArqsiArmario/ArqsiArmario/DTOs/AcabamentoDTO.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+namespace ArqsiArmario.DTOs
+{
+    public class AcabamentoDto
+    {
+
+        public string Nome { get; set; }
+
+    }
+}
---
+++ ArqsiArmario/ArqsiArmario/DTOs/CategoriaDTO.cs
@@ -0,0 +1,8 @@
+namespace ArqsiArmario.DTOs
+{
+    public class CategoriaDto
+    {
+        public string Nome { get; set; }
+
+    }
+}
