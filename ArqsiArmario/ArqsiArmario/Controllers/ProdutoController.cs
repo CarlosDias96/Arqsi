58c9bb609d3bdb51e730e9628d966dfd81e701a3
Diogo Soares <Diogomoraisoares@gmail.com>
2018-10-12 19:01:35 +0100

    Gets, create, update e delete de produto adicionados

--- ArqsiArmario/ArqsiArmario/Controllers/ProdutoController.cs
+++ ArqsiArmario/ArqsiArmario/Controllers/ProdutoController.cs
@@ -20,7 +20,7 @@ namespace TodoApi.Controllers
             {
                 // Create a new TodoItem if collection is empty,
                 // which means you can't delete all TodoItems.
-                _context.Produtos.Add(new Produto {});
+                _context.Produtos.Add(new Produto { Nome = "Item1" });
                 _context.SaveChanges();
             }
         }
@@ -29,9 +29,20 @@ namespace TodoApi.Controllers
         public ActionResult<List<Produto>> GetProdutos()
         {
             return _context.Produtos.ToList();
+
         }
 
-        [HttpGet("{id}", Name = "GetProduto")]
+        [HttpGet]
+        public IEnumerable<Produto> GetProdutosIEnum()
+        {
+            return _context.Produtos.ToList();
+        }
+
+
+
+
+
+        [HttpGet("{id}", Name = "GetProdutoById")]
         public ActionResult<Produto> GetProdutoById(int id)
         {
             var item = _context.Produtos.Find(id);
@@ -41,13 +52,104 @@ namespace TodoApi.Controllers
             }
             return item;
         }
+
+        [HttpGet("{nome}", Name = "GetProdutoByName")]
+        public ActionResult<Produto> GetProdutoByName(String nome)
+        {
+            var item = _context.Produtos.Find(nome);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        [HttpGet("{Produtos}", Name = "GetProdutoParts")]
+        public ActionResult<List<Produto>> GetProdutoParts(int id)
+        {
+            var item = _context.Produtos.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            List<Produto> P = (List<Produto>) item.Produtos;
+            return P;
+        }
+
+        //[HttpGet("{Produtos}", Name = "GetProdutoParteEM")]
+        //public ActionResult<List<Produto>> GetProdutosPai(int id)
+        //{
+        //    var item = _context.Produtos.Find(id);
+        //    if (item == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ActionResult<List<Produto>> resultado;
+        //    //ActionResult<List<Produto>> todos = GetProdutos();
+        //    //foreach (ActionResult<List<Produto>> A in todos)
+        //    //{
+
+        //    //}
+
+
+        //    //IEnumerable<List<Produto>> copia = GetProdutos();
+        //    foreach (ActionResult<List<Produto>> P in (IEnumerable<List<Produto>>)todos)
+        //    {
+        //        foreach (Produto P1 in P.getProdutos())
+        //        {
+        //            if (P1.getId() == item.getId())
+        //            {
+        //                resultado.add(P);
+        //            }
+        //        }
+        //    }
+
+        //    return resultado;
+        //}
+
+        [HttpGet("{Produtos}", Name = "GetProdutoParteEM")]
+        public ActionResult<List<Produto>> GetProdutosPai(int id)
+        {
+            var item = _context.Produtos.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            List<Produto> resultado = null;
+            IEnumerable<Produto> todos = GetProdutosIEnum();
+
+
+            foreach (IEnumerable<Produto> P in todos)
+            {
+                foreach (Produto P1 in P)
+                {
+                    if (P1.Id == item.Id)
+                    {
+                        //resultado.Add(P);  ainda sem solução
+                    }
+                }
+            }
+
+            return resultado;
+        }
+
+
+
         [HttpPost]
         public IActionResult Create(Produto item)
         {
             _context.Produtos.Add(item);
             _context.SaveChanges();
 
-            return CreatedAtRoute("GetProduto", new { id = item.Id }, item);
+            return CreatedAtRoute("GetProduto", new
+            {
+                id = item.Id,
+                nome = item.Nome,
+                produtos = item.Produtos,
+                material = item.Material,
+                categoria = item.Categoria,
+                categoriaID = item.Categoria
+            }, item);
         }
         [HttpPut("{id}")]
         public IActionResult Update(int id, Produto item)
@@ -60,7 +162,6 @@ namespace TodoApi.Controllers
 
             todo.Nome = item.Nome;
             todo.Material = item.Material;
-            todo.MaterialId = item.MaterialId;
             todo.Categoria = item.Categoria;
             todo.CategoriaId = item.CategoriaId;
             todo.Produtos = item.Produtos;
