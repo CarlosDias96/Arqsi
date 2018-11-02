d3d5d3069bf1035268200627a2b5317d0b72b285
Diogo Batista - 1150802 <1150802@isep.ipp.pt>
2018-11-02 19:59:14 +0000

    Criação base projeto IT2, mais ligação ao MongoDB

---
+++ ArqsiArmario/IT2/Encomendas/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const product = require('./routes/product.route'); // Imports routes for the products
+const app = express();
+app.use('/products', product);
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://gruponove:3mosqueteiros@ds249623.mlab.com:49623/encomendas_mongodb';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let port = 1234;
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
